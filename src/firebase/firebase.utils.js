5a07bcdcf12cf66ce97d24923d86f4fb5cc9152c
Alex-gits <alexeynb1@gmail.com>
2020-03-20 22:23:16 +0200

    implemented firebase utils, included ability to store users

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,34 @@ const config = {
   measurementId: "G-076WHFY0VS"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`); // Ссылка на путь к объекту внутри Фаербейс
+
+  const snapShot = await userRef.get(); // Получаем данные по указанной выше ссылке. Если юзер новый, то данных у него не будет, то есть снепшот вернет нулл (или типо того)
+
+  // проверяем, если у нас уже такой юзер (то есть чекам, есть ли у него данные). snapShot.exists сразу указывает, есть ли какие-то данные по ссылке. Если юзера нет (нет снепшота), то мы создаем в базе даных нового юзера по указанному референсу с 3 полями. Через .set()
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error while creating a user', error.message);
+    }
+  }
+
+  return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import SignIn from '../../components/sing-in/sign-in.component'
+import SignIn from '../../components/sing-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 import './sign-in-and-sign-up.styles.scss';
 
 const SignInAndSignUp = () => (
   <div className='sign-in-and-sign-up'>
     <SignIn />
+    <SignUp />
   </div>
 )
 
