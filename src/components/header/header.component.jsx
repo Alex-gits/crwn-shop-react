b991cb70ed5df596666db151781dd9367e438506
Alex-gits <alexeynb1@gmail.com>
2020-03-23 00:20:01 +0200

    added cart-icon component and implemented dropdown list functionality

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import { auth } from '../../firebase/firebase.utils'
+import { auth } from '../../firebase/firebase.utils';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropDown from '../cart-dropdown/cart-dropdown.component';
 
 import './header.styles.scss';
 
-const Header = ( {currentUser} ) => (
+const Header = ( {currentUser, hidden} ) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
@@ -29,12 +31,18 @@ const Header = ( {currentUser} ) => (
         :
         <Link className='option' to='/signin'>SIGN IN</Link>
       }
+      <CartIcon />
     </div>
+
+    {
+      hidden ? null : (<CartDropDown />)
+    }
   </div>
 );
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
+  currentUser,
+  hidden
 });
 
 export default connect(mapStateToProps)(Header);
---
+++ src/redux/cart/cart.actions.js
@@ -0,0 +1,7 @@
+import cartActionTypes from './cart.types';
+
+const toggleCartHidden = () => ({
+  type: cartActionTypes.TOGGLE_CART_HIDDEN
+});
+
+export default toggleCartHidden;
