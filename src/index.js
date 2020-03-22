58279f0f905a1d22632ec632bc057dcef866b024
Alex-gits <alexeynb1@gmail.com>
2020-03-22 22:48:29 +0200

    updating the app to redirect to home if user is signed in and trying to access the sigin page

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 
-import './index.css';
-
 import store from './redux/store';
+
+import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>, 
+  </Provider>,
   document.getElementById('root')
 );
